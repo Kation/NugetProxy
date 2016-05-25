b42fac7009c8fee4d37f026d43256189632c7591
Kation <lwcqq@163.com>
2016-05-25 10:33:01 +0800

    更新前端

--- src/Wodsoft.NugetProxy/Controllers/HomeController.cs
+++ src/Wodsoft.NugetProxy/Controllers/HomeController.cs
@@ -3,27 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Wodsoft.NugetProxy.Models;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
 
 namespace Wodsoft.NugetProxy.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            DataContext context = new DataContext();
+            ViewBag.PageCount = await context.Page.CountAsync();
+            ViewBag.PackageCount = new DirectoryInfo("Packages").GetFiles().Length;
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Help()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
